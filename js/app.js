d370b4c5ea8f3882b54b8ba24080bd1d23f2e608
tianye <838199892@qq.com>
2017-04-04 21:36:58 +0800

    404

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 $(document).ready(function(){
 	var lignts = $('.deathlight');
 	lignts.click(function(){
+		if ($(this).hasClass('diasbled')) {
+			return false;
+		}else{
+			if ($) {}
+		}
 		$(this).addClass('off');
 	})
 });
