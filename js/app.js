408f625105bc9fefa99c95eefcfde5b426395802
tianye19941001 <838199892@qq.com>
2017-04-05 13:40:13 +0800

    tt

--- js/app.js
+++ js/app.js
@@ -2,11 +2,28 @@
 $(document).ready(function(){
 	var lignts = $('.deathlight');
 	lignts.click(function(){
-		if ($(this).hasClass('diasbled')) {
-			return false;
+		if ($(this).parent().hasClass('active')) {
+			if ($(this).hasClass('diasbled')) {
+				return false;
+			}else{
+				if($(this).hasClass('off')) {
+					$(this).removeClass('off').addClass('on');
+				}else{
+					$(this).removeClass('on').addClass('off');
+				}
+			}
 		}else{
-			if ($) {}
+			
+			$(this).parent().addClass('active').siblings().removeClass('active');
+			if ($(this).hasClass('diasbled')) {
+				return false;
+			}else{
+				if($(this).hasClass('off')) {
+					$(this).removeClass('off').addClass('on');
+				}else{
+					$(this).removeClass('on').addClass('off');
+				}
+			}
 		}
-		$(this).addClass('off');
 	})
 });
